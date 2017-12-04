47a9991f0b4ca97d6df3d382ffacccc93a827776
Chelsea Weibel <cweibel2@gmail.com>
2017-12-04 16:33:35 -0500

    I think I have the data for the regression and 2-level anova figured out!

---
+++ ANOVA.R
@@ -0,0 +1,21 @@
+#### ANOVA ####
+x = c(13,13,13,13,13,13,13,13,13,13,13,13,38,38,38,38,38,38,38,38,38,38,38,38)
+x_values = matrix(rep(x,10),nrow=24,ncol=10)
+num_sims <- 10 # Simulate the data 10 times 
+n_per_sim <- 24 # Each simulation will have 24 values
+set.seed(24) # Seed is necessary for reproducibility 
+sigma_num <- 8
+sigma <- c(1,2,4,6,8,12,16,24)
+
+y <- .4 * x_values + 10
+
+outputy<-as.data.frame(matrix(0, ncol=10, nrow=24))
+youtput <-list()
+
+  for (sigma_number in 1:sigma_num){
+    outputy = y + rnorm(length(y), mean=0, sd=sigma[sigma_number])
+    
+    youtput[[sigma_number]] <- outputy
+  }
+
+youtput[[1]]
